7550f78aa591cb8398dc9e10b2ab086a5e7e87fb
HieuPahm <phamvanthang786@gmail.com>
2026-07-05 15:44:42 +0700

    Accept standard CAPTCHA response field

--- src/main/java/com/vietnam/pji/dto/request/ForgotPasswordRequestDTO.java
+++ src/main/java/com/vietnam/pji/dto/request/ForgotPasswordRequestDTO.java
@@ -1,5 +1,7 @@
 package com.vietnam.pji.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -12,5 +14,6 @@ public class ForgotPasswordRequestDTO {
     @Email(message = "email không hợp lệ")
     private String email;
 
+    @JsonAlias({ "cf-turnstile-response", "g-recaptcha-response" })
     private String captchaToken;
 }
